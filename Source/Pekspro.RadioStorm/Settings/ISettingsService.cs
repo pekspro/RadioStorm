1e48c00554c23a2402f8707a4093f472cd08c242
pekspro <msn@pekspro.com>
2022-07-20 12:05:35 +0200

    In OnPlatform, replaces UWP with WinUI.

--- Source/Pekspro.RadioStorm/Settings/ISettingsService.cs
+++ Source/Pekspro.RadioStorm/Settings/ISettingsService.cs
@@ -22,37 +22,3 @@ public interface ISettingsService
 
     T GetSafeValue<T>(string key, T defaultValue);
 }
-
-/* UWP-version
- 
-/// <summary>
-/// A simple <see langword="class"/> that handles the local app settings.
-/// </summary>
-public sealed class SettingsService : ISettingsService
-{
-    /// <summary>
-    /// The <see cref="IPropertySet"/> with the settings targeted by the current instance.
-    /// </summary>
-    private readonly IPropertySet SettingsStorage = ApplicationData.Current.LocalSettings.Values;
-
-    /// <inheritdoc/>
-    public void SetValue<T>(string key, T value)
-    {
-        if (!SettingsStorage.ContainsKey(key)) SettingsStorage.Add(key, value);
-        else SettingsStorage[key] = value;
-    }
-
-    /// <inheritdoc/>
-    public T GetValue<T>(string key)
-    {
-        if (SettingsStorage.TryGetValue(key, out object value))
-        {
-            return (T)value;
-        }
-
-        return default;
-    }
-}
-
- */
-
