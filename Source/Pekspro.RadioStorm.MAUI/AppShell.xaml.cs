2546bf4cb4ebd1788512ef742c403dd21c2ea8cb
pekspro <msn@pekspro.com>
2022-07-31 20:42:23 +0200

    Removes obselete flyout fix.

--- Source/Pekspro.RadioStorm.MAUI/AppShell.xaml.cs
+++ Source/Pekspro.RadioStorm.MAUI/AppShell.xaml.cs
@@ -7,17 +7,6 @@ public partial class AppShell : Shell
         InitializeComponent();
     }
 
-    async protected override void OnAppearing()
-    {
-        base.OnAppearing();
-
-        await Task.Delay(2200);
-
-        var color = FlyoutBackgroundColor;
-        FlyoutBackgroundColor = Colors.DarkBlue;
-        FlyoutBackgroundColor = color;
-    }
-
     async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync(nameof(SettingsPage));
