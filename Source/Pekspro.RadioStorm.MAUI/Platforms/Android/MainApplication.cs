c0e3dc4c663e20bf455a1fa919515f74707dc91d
pekspro <msn@pekspro.com>
2022-08-21 10:53:53 +0200

    Fixes incorrect permission.

--- Source/Pekspro.RadioStorm.MAUI/Platforms/Android/MainApplication.cs
+++ Source/Pekspro.RadioStorm.MAUI/Platforms/Android/MainApplication.cs
@@ -1,10 +1,11 @@
 ﻿using Android.App;
 using Android.Runtime;
 
+[assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
+
 namespace Pekspro.RadioStorm.MAUI;
 
 [Application]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
 public class MainApplication : MauiApplication
 {
     public MainApplication(IntPtr handle, JniHandleOwnership ownership)
