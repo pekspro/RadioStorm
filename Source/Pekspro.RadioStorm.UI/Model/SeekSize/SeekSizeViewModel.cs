8c2133f1c50d03a022fc9924f60bdcd65fe9c076
pekspro <msn@pekspro.com>
2022-08-24 20:23:41 +0200

    Adds SeekSizeViewModel.

---
+++ Source/Pekspro.RadioStorm.UI/Model/SeekSize/SeekSizeViewModel.cs
@@ -0,0 +1,45 @@
+﻿namespace Pekspro.RadioStorm.UI.Model.SeekSize;
+
+public class SeekSizeViewModel : ObservableObject
+{
+    #region Private properties
+
+    private SeekSizeProvider SeekSizeProvider = new SeekSizeProvider();
+
+    #endregion
+
+    #region Constructor
+
+    public SeekSizeViewModel()
+    {
+        SeekSizeProvider.SeekPositionChanged = () =>
+        {
+            OnPropertyChanged(nameof(RewindSize));
+            OnPropertyChanged(nameof(ForwardSize));
+        };
+    }
+
+    #endregion
+
+    #region Properties
+
+    public TimeSpan RewindSize => SeekSizeProvider.RewindSize;
+
+    public TimeSpan ForwardSize => SeekSizeProvider.ForwardSize;
+
+    #endregion
+
+    #region Methods
+
+    public void Increase()
+    {
+        SeekSizeProvider.Increase();
+    }
+
+    public void Decrease()
+    {
+        SeekSizeProvider.Decrease();
+    }
+
+    #endregion
+}
---
+++ Source/Pekspro.RadioStorm/Utilities/SeekSizeProvider.cs
@@ -0,0 +1,95 @@
+﻿namespace Pekspro.RadioStorm.Utilities;
+
+public class SeekSizeProvider
+{
+
+    public static List<TimeSpan> SeekSizes = new List<TimeSpan>()
+    {
+        TimeSpan.FromSeconds(5),
+        TimeSpan.FromSeconds(10),
+        TimeSpan.FromSeconds(20),
+        TimeSpan.FromSeconds(30),
+        TimeSpan.FromMinutes(1),
+        TimeSpan.FromMinutes(2),
+        TimeSpan.FromMinutes(5)
+    };
+
+    public TimeSpan RewindSize
+    {
+        get
+        {
+            if (SeekPosition <= 0)
+            {
+                return -SeekSizes[-SeekPosition];
+            }
+
+            return -SeekSizes[SeekPosition - 1];
+        }
+    }
+
+    public TimeSpan ForwardSize
+    {
+        get
+        {
+            if( SeekPosition >= 0)
+            {
+                return SeekSizes[SeekPosition];
+            }
+
+            int reversedPos = -SeekPosition;
+
+            return SeekSizes[reversedPos - 1];
+        }
+    }
+
+    public Action? SeekPositionChanged;
+
+    private int _SeekPosition;
+    
+    public int SeekPosition
+    {
+        get => _SeekPosition;
+        
+        private set
+        {
+            if (_SeekPosition != value)
+            {
+                _SeekPosition = value;
+                SeekPositionChanged?.Invoke();
+            }
+        }
+    }
+
+    public void Increase()
+    {
+        Change(1);
+    }
+
+    public void Decrease()
+    {
+        Change(-1);
+    }
+
+    private int ChangeId;
+
+    private async void Change(int v)
+    {
+        ChangeId++;
+     
+        int currentChangeId = ChangeId;
+
+        int newPos = Math.Clamp(SeekPosition + v, -SeekSizes.Count + 1, SeekSizes.Count - 1);
+
+        if (SeekPosition != newPos)
+        {
+            SeekPosition = newPos;
+        }
+
+        await Task.Delay(2000);
+
+        if (currentChangeId == ChangeId)
+        {
+            SeekPosition = 0;
+        }
+    }
+}
