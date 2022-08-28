bbb6da625b494825d0a6ae2e0e96ab7ed6b18984
pekspro <msn@pekspro.com>
2022-08-28 10:17:52 +0200

    Hides notification during crash.

--- Source/Pekspro.RadioStorm.MAUI/Platforms/Android/MainApplication.cs
+++ Source/Pekspro.RadioStorm.MAUI/Platforms/Android/MainApplication.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Runtime;
+using Pekspro.RadioStorm.MAUI.Platforms.Android.Services;
 
 [assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
 
@@ -14,6 +15,8 @@ public class MainApplication : MauiApplication
         AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
         TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
         AndroidEnvironment.UnhandledExceptionRaiser += AndroidEnvironment_UnhandledExceptionRaiser;
+        
+        // NotificationHelper.StopNotification(this);
     }
 
     private ILogger? _Logger;
@@ -29,22 +32,26 @@ public class MainApplication : MauiApplication
     private void AndroidEnvironment_UnhandledExceptionRaiser(object? sender, RaiseThrowableEventArgs e)
     {
         Logger.LogError(e.Exception, nameof(AndroidEnvironment_UnhandledExceptionRaiser));
+        NotificationHelper.StopNotification(this);
     }
 
     private void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
     {
         Logger.LogError(e.Exception, nameof(TaskScheduler_UnobservedTaskException));
+        NotificationHelper.StopNotification(this);
     }
 
     private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
     {
         Logger.LogError(e.ExceptionObject as Exception, nameof(CurrentDomain_UnhandledException));
+        NotificationHelper.StopNotification(this);
     }
 
     public override void OnTerminate()
     {
-        Logger.LogWarning($"Is terminating");
-        
+        Logger.LogWarning($"Is terminating.");
+        NotificationHelper.StopNotification(this);
+
         base.OnTerminate();
     }
 
