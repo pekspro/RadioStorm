0511b6c8910ea3b4dcb8a2ad5e56ddac713356b0
pekspro <msn@pekspro.com>
2022-09-29 18:20:05 +0200

    Adds more information into BuildInformation.

--- Source/Pekspro.RadioStorm/Utilities/BuildInformation.cs
+++ Source/Pekspro.RadioStorm/Utilities/BuildInformation.cs
@@ -9,10 +9,22 @@ public static partial class BuildInformation
 
     static partial void Initialize();
 
+    public static string? Branch { get; private set; }
+
     public static string? BuildTimeString { get; private set; }
 
     public static DateTime BuildTime =>
         BuildTimeString is null ? DateTime.MinValue : DateTime.Parse(BuildTimeString);
+
+    public static string? DotNetVersionString { get; private set; }
+
+    public static string? MauiWorkloadAndroidVersionString { get; private set; }
+
+    public static string? MauiWorkloadWindowsVersionString { get; private set; }
+
+    public static string? MauiWorkloadIosVersionString { get; private set; }
+
+    public static string? MauiWorkloadMacCatalysVersionString { get; private set; }
 }
 
 /*
@@ -22,7 +34,13 @@ public static sealed partial class BuildInformation
 {
     static partial void Initialize()
     {
+        Branch = "main";
         BuildTimeString = "2022-07-16T18:41:12Z";
+        DotNetVersionString = "2022-07-16T18:41:12Z";
+        MauiWorkloadAndroidVersionString = "6.0.486/6.0.400";
+        MauiWorkloadWindowsVersionString = "6.0.486/6.0.400";
+        MauiWorkloadIosVersionString = "6.0.486/6.0.400";
+        MauiWorkloadMacCatalysVersionString = "6.0.486/6.0.400";
     }
 }
 */
