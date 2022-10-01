d013fa7dc5285c3987e5b58838fcbd79ab248b6e
pekspro <msn@pekspro.com>
2022-10-01 22:38:11 +0200

    Add not listened episode count into album view.

---
+++ Source/Pekspro.RadioStorm.MAUI/Converters/LargerThanZeroConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace Pekspro.RadioStorm.MAUI.Converters;
+
+public sealed class LargerThanZeroConverter : IValueConverter
+{
+    public LargerThanZeroConverter()
+    {
+    }
+
+    public bool TrueValue { get; set; } = true;
+    
+    public bool FalseValue { get; set; } = false!;
+    
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is int i && i > 0)
+        {
+            return TrueValue;
+        }
+
+        return FalseValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
