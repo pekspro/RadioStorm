b745cee6a76c2681e9f659fbb72fdcaeb9b7ee30
pekspro <msn@pekspro.com>
2022-10-01 14:22:54 +0200

    Hides favorite star on channel control when in favorite list.

--- Source/Pekspro.RadioStorm.MAUI/Pages/Channel/ChannelControl.xaml.cs
+++ Source/Pekspro.RadioStorm.MAUI/Pages/Channel/ChannelControl.xaml.cs
@@ -9,5 +9,22 @@ public sealed partial class ChannelControl
         WidthStateHelper.ConfigureWidthState(this);
     }
 
-    private ChannelModel ViewModel => (ChannelModel) BindingContext;
+    private ChannelModel ViewModel => (ChannelModel)BindingContext;
+
+    public static readonly BindableProperty FavoriteModeProperty =
+        BindableProperty.Create(nameof(FavoriteMode), typeof(bool), typeof(ChannelControl), null, BindingMode.OneWay, null, OnFavoriteModeChanged);
+
+    private static void OnFavoriteModeChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is ChannelControl control)
+        {
+            control.FavoriteImageHolder.IsVisible = false;
+        }
+    }
+
+    public bool FavoriteMode
+    {
+        get { return (bool)GetValue(FavoriteModeProperty); }
+        set { SetValue(FavoriteModeProperty, value); }
+    }
 }
