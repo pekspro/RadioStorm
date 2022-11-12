179b6949d922e07943f59e09a412742707332b0d
pekspro <msn@pekspro.com>
2022-11-12 12:12:01 +0100

    Updates workaround for maui issue 10452.

---
+++ Source/Pekspro.RadioStorm.MAUI/Utilities/Workarounds.cs
@@ -0,0 +1,25 @@
+﻿namespace Pekspro.RadioStorm.MAUI.Utilities;
+
+public static class Workarounds
+{
+    // TODO: Remove when fixed: https://github.com/dotnet/maui/issues/10452
+    public static void FixToolbarItems(this ContentPage page)
+    {
+#if ANDROID
+        var secondayToolBarItems = page.ToolbarItems.Where(x => x.Order == ToolbarItemOrder.Secondary).ToList();
+
+        foreach (var item in secondayToolBarItems)
+        {
+            page.ToolbarItems.Remove(item);
+        }
+
+        Application.Current!.Dispatcher.Dispatch(() =>
+        {
+            foreach (var item in secondayToolBarItems)
+            {
+                page.ToolbarItems.Add(item);
+            }
+        });
+#endif
+    }
+}
